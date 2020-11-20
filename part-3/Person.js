05e98901d8ec0ed6d0a6eb59f76f0321a5f344ce
Trey McDeane <tmcdeane@gmail.com>
2020-11-20 15:47:11 -0500

    forgot hobbies

--- part-3/Person.js
+++ part-3/Person.js
@@ -4,5 +4,10 @@ const Person = ({ name, age, hobbies }) => (
         <p><b>Name:</b> {name.length > 8 ? name.slice(0, 6) : name}</p>
         <p><b>Age:</b> {age}</p>
         <h3>{age >= 18 ? 'Please go vote!' : 'You must be 18'}</h3>
+        <h3>Hobbies:</h3>
+        <ul>
+            {hobbies.map(h => <li>{h}</li>)}
+        </ul>
+        <hr/>
     </div>
 );
